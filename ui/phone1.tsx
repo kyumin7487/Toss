672ce746600bf7e994823c85793d9f91148c4cda
박규민 <131234408+kyumin7487@users.noreply.github.com>
2025-05-06 15:37:54 +0900

    feat :: phone

---
+++ ui/phone1.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function OverlayImages() {
+    return (
+        <div className="relative w-full h-full">
+            <Image
+                src="/home/home_screen_1.png"
+                alt="Home Screen"
+                width={342}
+                height={741}
+                className="absolute top-25 left-33 z-1"
+            />
+            <Image
+                src="/iPhone15_Clay_Shadow_03.png"
+                alt="iPhone Frame"
+                width={600}
+                height={936}
+                className="absolute top-0 left-0 z-20"
+            />
+        </div>
+    );
+}
